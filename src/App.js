ffff21033ccd135ac5695f33176a40ba6721ece2
rtake <rtake1110@gmail.com>
2023-08-01 02:05:04 +0900

    ポケモンの詳細情報を取得する非同期処理を追加

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { getAllPokemon } from './utils/pokemon.js';
+import { getAllPokemon, getPokemon } from './utils/pokemon.js';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon"
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([])
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      loadPokemon(res.results);
+      console.log(res.results);
       setLoading(false);
     };
     fetchPokemonData();
   }, []);
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon) => {
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    )
+    setPokemonData(_pokemonData)
+  }
+
+  console.log(pokemonData)
 
   return (
     <div className="App">
