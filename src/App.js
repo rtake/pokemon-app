9bca478dbc8a8cdf8aec84d6dd7fe7428055e29f
rtake <rtake1110@gmail.com>
2023-08-01 02:56:25 +0900

    Navbarを追加

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon, getPokemon } from './utils/pokemon.js';
 import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon"
@@ -31,16 +32,19 @@ function App() {
   console.log(pokemonData)
 
   return (
-    <div className="App">
-      {loading ? (
-        <h1>laoding.../</h1>
-      ) : (
-        <div className='pokemonCardContainer'>
-          {pokemonData.map((pokemon, i) => {
-            return <Card key={i} pokemon={pokemon} />
-          })}
-        </div>
-      )}
+    <div>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>laoding.../</h1>
+        ) : (
+          <div className='pokemonCardContainer'>
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />
+            })}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
