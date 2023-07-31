d66d096058f8b179c9c3400ed68ad414b8dbf9ac
rtake <rtake1110@gmail.com>
2023-08-01 01:52:40 +0900

    データ読み込み中の状態を管理するloading変数を追加

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon } from './utils/pokemon.js';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon"
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      {loading ? (<h1>laoding.../</h1>) : <h1>complete!</h1>}
+    </div>
+  )
 }
 
 export default App;
